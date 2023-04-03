1f7ee97630a7dc98f06365c404be61ce7bc135f6
MoniIgnacio <moniignacio02@gmail.com>
2023-04-03 14:07:17 +0200

    valid email check & password security done

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,9 +15,43 @@ exports.getAllUsers = async (req, res) => {
 };
 
 exports.createUser = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password, passwordSecure } = req.body;
 
-  const query = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
+  if (email === "" || password === "" || passwordSecure === "") {
+    res.status(400).json({ errorMessage: "All fields need to be filled" });
+    return;
+  }
+
+  // password 1 === password 2
+  if (password !== passwordSecure) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please make sure both passwords are the same" });
+    return;
+  }
+
+  // Password security
+
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/gm;
+  if (passwordRegex.test(password) === false) {
+    res.status(400).json({
+      errorMessage:
+        "The password should contain at least 6 characters, one uppercase letter and a number or special character",
+    });
+    return;
+  }
+
+  //valid email check
+  const emailRegex =
+    /[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  if (emailRegex.test(email) === false) {
+    res
+      .status(400)
+      .json({ errorMessage: "The e-mail needs to be a valid direction" });
+    return;
+  }
+
+  const query = `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`;
 
   conn.query(query, (err, result) => {
     if (err) {
